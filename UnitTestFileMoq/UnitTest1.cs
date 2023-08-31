58bd9784c177c71b0b299f131e5bd55991668087
Shakeeb <shakeebparavannur@gmail.com>
2023-08-31 18:11:14 +0530

    AddProductTest

--- UnitTestFileMoq/UnitTest1.cs
+++ UnitTestFileMoq/UnitTest1.cs
@@ -48,6 +48,38 @@ namespace UnitTestFileMoq
             Assert.True(productList[1].ProductId == productResult.ProductId);
         }
 
+        [Theory]
+        [InlineData("IPhone")]
+        public void CheckProductExistOrNotByProductName_Product(string productName)
+        {
+            //arrange
+
+            var productList = GetProductsData();
+            productService.Setup(x=>x.GetProductList())
+                .Returns(productList);
+
+            var productController = new ProductController(productService.Object);
+
+            var productResult = productController.ProductList();
+            var expectedProductName = productResult.ToList()[0].ProductName;
+
+            Assert.Equal(productName,expectedProductName);
+        }
+        [Fact]
+        public void AddProduct_Product()
+        {
+            var productList = GetProductsData();
+            productService.Setup(x => x.AddProduct(productList[2]))
+                .Returns(productList[2]);
+            var productController = new ProductController(productService.Object);
+
+            var productResult = productController.AddProduct(productList[2]);
+
+            Assert.NotNull(productResult);
+            Assert.Equal(productList[2].ProductId, productResult.ProductId);
+            Assert.True(productList[2].ProductId==productResult.ProductId);
+
+        }
 
         private List<Product> GetProductsData()
         {
