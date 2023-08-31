03e1fe0ff97688503de3405e8e95d90f619a32f9
Shakeeb <shakeebparavannur@gmail.com>
2023-08-31 16:36:59 +0530

    Add project files.

---
+++ UnitTestFileMoq/UnitTest1.cs
@@ -0,0 +1,84 @@
+using Moq;
+using UnitTestMoq.Controllers;
+using UnitTestMoq.Models;
+using UnitTestMoq.Services;
+
+namespace UnitTestFileMoq
+{
+    public class UnitTestController
+    {
+        private readonly Mock<IProductService> productService;
+        public UnitTestController()
+        {
+            productService = new Mock<IProductService>();
+        }
+        [Fact]
+        public void GetProductList_ProductList()
+        {
+            //arrange
+
+            var productList = GetProductsData();
+            productService.Setup(x=>x.GetProductList())
+                .Returns(productList);
+            var productController = new ProductController(productService.Object);
+
+            //act
+            var productResult = productController.ProductList();
+
+            //assert
+
+            Assert.NotNull(productResult);
+            Assert.Equal(GetProductsData().Count, productResult.Count());
+            Assert.Equal(GetProductsData().ToString(), productResult.ToString());
+            Assert.True(productList.Equals(productResult));
+
+        }
+        [Fact]
+        public void GetProductById_ProductById()
+        {
+            var productList = GetProductsData();
+            productService.Setup(x => x.GetProductById(2))
+                .Returns(productList[1]);
+            var productController = new ProductController(productService.Object);
+
+            var productResult = productController.GetProductById(2);
+
+            Assert.NotNull(productResult);
+            Assert.Equal(productList[1].ProductId, productResult.ProductId);
+            Assert.True(productList[1].ProductId == productResult.ProductId);
+        }
+
+
+        private List<Product> GetProductsData()
+        {
+            List<Product> productsData = new List<Product>
+        {
+            new Product
+            {
+                ProductId = 1,
+                ProductName = "IPhone",
+                ProductDescription = "IPhone 12",
+                ProductPrice = 55000,
+                ProductStock = 10
+            },
+             new Product
+            {
+                ProductId = 2,
+                ProductName = "Laptop",
+                ProductDescription = "HP Pavilion",
+                ProductPrice = 100000,
+                ProductStock = 20
+            },
+             new Product
+            {
+                ProductId = 3,
+                ProductName = "TV",
+                ProductDescription = "Samsung Smart TV",
+                ProductPrice = 35000,
+                ProductStock = 30
+            },
+        };
+            return productsData;
+        }
+    }
+}
---
+++ UnitTestMoq/Data/DbContextClass.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UnitTestMoq.Models;
+
+namespace UnitTestMoq.Data
+{
+    public class DbContextClass:DbContext
+    {
+        
+        public DbContextClass(DbContextOptions<DbContextClass>options):base(options)
+        {
+            
+        }
+        
+        public DbSet<Product> Products { get; set; }
+    }
+}
